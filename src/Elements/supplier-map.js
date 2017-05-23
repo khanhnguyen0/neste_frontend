b8cb584baee502cbf80f599bf54c1e3fa75cbccf
Khanh Nguyen <khanh@rnd.works>
2017-05-23 12:18:04 +0300

    added google maps to build

--- src/Elements/supplier-map.js
+++ src/Elements/supplier-map.js
@@ -8,9 +8,11 @@ const containerStyle = {
 
 const SupplierMap = (props) =>{
   const {list} = props;
+  const zoom = list.length>1?2:6;
+  const center = list.length>1?null:list[0].position;
   return (
     <div className = "supplier-map">
-      <Map google={window.google} containerStyle = {containerStyle} zoom = {2} >
+      <Map google={window.google} containerStyle = {containerStyle} zoom = {zoom} mapCenter = {center} >
       {list.length>0?list.map((s,k)=><Marker title = {s.supplier_name} key = {k} position = {s.position} />):null}
     </Map>
     </div>
